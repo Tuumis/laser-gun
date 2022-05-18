84a2025a4bec4b7772471d77faf042f9b309a7bf
Tuumis <37293673+Tuumis@users.noreply.github.com>
2022-05-18 14:03:42 +0300

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,20 @@
+Void setup() {
+    //Selecting fire mode etc.
+}
+
+
+Void loop() {
+      //main code that is looped
+    if (/*trigger is pressed*/) {
+        shoot();
+    }
+}
+
+Void shoot() {
+    //blink the led
+}
+
+Void changeMag() {
+    //Change mag
+}
+
